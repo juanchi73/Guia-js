7d772427f1f47dc41c7cedf2eb6dc22bd86a86f1
juanchi73 <82169942+juanchi73@users.noreply.github.com>
2026-04-29 16:39:54 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,15 @@
 console.log("Guía JavaScript iniciada");
+//Ejercicio 1
+const nombre = "Juan";
+let edad = 23;
+let Vida = 100;
+let puntaje = 0;
+let esta_activo = true;
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Vida:", Vida);
+console.log("Puntaje:", puntaje);
+console.log("¿Está activo?", esta_activo);
+
+//Ejercicio 2
